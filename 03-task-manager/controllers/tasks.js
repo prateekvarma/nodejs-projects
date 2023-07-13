d2089bf65b49e7c3876b32ce0369336dec2887db
prateekvarma <prtk.varma@gmail.com>
2023-07-13 15:38:27 -0600

    Controllers alterations

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  res.send('create item');
+  res.json(req.body );
 };
 
 const getTask = (req, res) => {
-  res.send('get single item');
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
