ab4449e575f09fd3289bd347febcbfd84a75c26c
prateekvarma <prtk.varma@gmail.com>
2023-07-21 15:13:30 -0600

    Used mongoose to connect

--- 03-task-manager/db/connect.js
+++ 03-task-manager/db/connect.js
@@ -1,1 +1,14 @@
-const connectionString = "mongodb+srv://<username>:<password>@cluster0.xgnakrk.mongodb.net/?retryWrites=true&w=majority"
+const mongoose = require('mongoose');
+
+const connectionString =
+  'mongodb+srv://<username>:<password>@cluster0.xgnakrk.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to DB...'))
+  .catch((err) => console.log(err));
