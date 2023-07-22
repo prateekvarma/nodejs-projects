5bc4e56b0797bddfe91dce9f76fa6c4e4f706713
prateekvarma <prtk.varma@gmail.com>
2023-07-22 12:10:51 -0600

    Used dotenv

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -1,8 +1,9 @@
-require('./db/connect')
-
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+
+const connectDB = require('./db/connect');
+require('dotenv').config()
 
 const port = 3001;
 
@@ -14,7 +15,15 @@ app.get('/hello', (req, res) => {
   res.send('Task Manager App');
 });
 
+app.use('/api/v1/tasks', tasks);
 
-app.use('/api/v1/tasks', tasks)
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+start()
--- 03-task-manager/db/connect.js
+++ 03-task-manager/db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-  'mongodb+srv://<username>:<password>@cluster0.xgnakrk.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to DB...'))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
