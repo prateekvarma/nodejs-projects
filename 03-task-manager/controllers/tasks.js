f6dc3875fdaff742f21d6be0ebad7f0dc98ba032
prateekvarma <prtk.varma@gmail.com>
2023-08-02 11:37:27 -0600

    Update task conttroller

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -1,4 +1,4 @@
-const Task = require('../models/Task');
+const Task = require("../models/Task");
 
 const getAllTasks = async (req, res) => {
   try {
@@ -18,16 +18,34 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 const updateTask = (req, res) => {
-  res.send('update item');
+  res.send("update item");
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete item');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 module.exports = {
