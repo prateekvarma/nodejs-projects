d3fabc40329b03f9221eb2e69922061d2b107833
prateekvarma <prtk.varma@gmail.com>
2023-08-02 12:05:06 -0600

    Update function in controller

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -31,8 +31,20 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update item");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    }); // the { new: true } is not necessary, but it helps get the fresh values instead of stale values. Its the options request.
+    if (!task) {
+      return res.status(404).json({ message: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
