1fc98e539c3d6b5c1dc4f7a660fdb9e4dbd74d4a
prateekvarma <prtk.varma@gmail.com>
2023-08-06 14:29:40 -0600

    Middleware for 404

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require('./routes/tasks');
 
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
 require('dotenv').config()
 
 const port = 3001;
@@ -14,6 +15,7 @@ app.use(express.json());
 //routes
 
 app.use('/api/v1/tasks', tasks);
+app.use(notFound)
 
 const start = async () => {
   try {
